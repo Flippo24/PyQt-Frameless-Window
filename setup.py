7d9e116d2a2f90e4280e52b47ae7126918e20a45
Flippo24 <Flippo24@users.noreply.github.com>
2024-12-04 08:35:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,37 @@
+import platform
 import setuptools
 
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
+install_requires = []
+
+match platform.system():
+    case "Darwin":
+        install_requires.append("pyobjc")
+        install_requires.append("PyCocoa")
+    case "Windows":
+        install_requires.append("pywin32")
+    case "Linux":
+        install_requires.append("xcffib")
+
 setuptools.setup(
-    name="PySideSix-Frameless-Window",
+    name="qframelesswindow",
     version="0.4.3",
-    keywords="pyside6 frameless",
+    keywords="pyqt6 pyside6 frameless",
     author="zhiyiYo",
     author_email="shokokawaii@outlook.com",
-    description="A cross-platform frameless window based on pyside6, support Win32, Linux and macOS.",
+    description="A cross-platform frameless window based on qt6, support Win32, X11, Wayland and macOS.",
     long_description=long_description,
     long_description_content_type='text/markdown',
-    license="LGPLv3",
-    url="https://github.com/zhiyiYo/PyQt-Frameless-Window/tree/PySide6",
+    license="GPLv3",
+    url="https://github.com/Flippo24/qframelesswindow/tree/PySide6",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "pywin32;platform_system=='Windows'",
-        "pyobjc;platform_system=='Darwin'",
-        "PyCocoa;platform_system=='Darwin'",
-    ],
+    install_requires=install_requires,
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent'
     ]
 )
